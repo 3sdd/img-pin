89f1265ef8c13c2ceebe043ec95f4d0ea0dbda4b
msdd <56486103+3sdd@users.noreply.github.com>
2024-07-06 00:06:19 +0900

    Update MenuBar.vue

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,68 +1,96 @@
 <script setup lang="ts">
-import { appWindow } from '@tauri-apps/api/window'
-import { onMounted, ref } from 'vue';
+import { appWindow } from "@tauri-apps/api/window";
+import { onMounted, ref } from "vue";
 
-const isWindowMaximized=ref(false);
-onMounted(async ()=>{
-  isWindowMaximized.value=await appWindow.isMaximized()
-})
-
-const alwaysOnTop=ref(false);
-function onPinClick(){
-    alwaysOnTop.value=!alwaysOnTop.value
-    appWindow.setAlwaysOnTop(alwaysOnTop.value);
+const alwaysOnTop = ref(false);
+function onPinClick() {
+  alwaysOnTop.value = !alwaysOnTop.value;
+  appWindow.setAlwaysOnTop(alwaysOnTop.value);
 }
-function onMinimizeClick(){
-    appWindow.minimize()
+function onMinimizeClick() {
+  appWindow.minimize();
 }
 
-function onMaximizeClick(){
-    appWindow.toggleMaximize()
+function onMaximizeClick() {
+  appWindow.toggleMaximize();
 }
-function onCloseClick(){
-    appWindow.close()
+function onCloseClick() {
+  appWindow.close();
 }
 
-
+const fileMenu = ["Open", "OpenRecent"];
 </script>
 
 <template>
-    <div data-tauri-drag-region class="titlebar">
-    <div class="titlebar-button" id="titlebar-pin" @click="onPinClick">
-    <img
-        src="https://api.iconify.design/mdi:pin-off.svg"
-        alt="pin-off"
-        v-if="alwaysOnTop"
-    />
-    <img
-        src="https://api.iconify.design/mdi:pin.svg"
-        alt="pin-on"
-        v-else
-    />
-</div>
-  <div class="titlebar-button" id="titlebar-minimize" @click="onMinimizeClick">
-      <img
-      src="https://api.iconify.design/mdi:window-minimize.svg"
-      alt="minimize"
-      />
-  </div>
-  <div class="titlebar-button" id="titlebar-maximize" @click="onMaximizeClick">
-      <img
-      src="https://api.iconify.design/mdi:window-maximize.svg"
-      alt="maximize"
-      />
-      <img
-      src="https://api.iconify.design/mdi:window-restore.svg"
-      alt="restore"
-      />
-      {{ isWindowMaximized }}
-  </div>
-  <div class="titlebar-button" id="titlebar-close" @click="onCloseClick">
-      <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
-  </div>
+  <div data-tauri-drag-region class="titlebar">
+    <div class="titlebar-left flex-none">
+      <button class="bg-gray-200 px-2">File</button>
+      <button class="bg-gray-200 px-2">Edit</button>
+    </div>
+
+    <div
+      x-show="dropdownOpen"
+      class="absolute left-0 top-5 mt-2 w-48 bg-white rounded-md overflow-hidden shadow-xl z-20"
+    >
+      <a
+        href="#"
+        class="block px-4 py-2 text-sm text-gray-800 border-b hover:bg-gray-200"
+        >Open</a
+      >
+      <a
+        href="#"
+        class="block px-4 py-2 text-sm text-gray-800 border-b hover:bg-gray-200"
+        >Open Recent</a
+      >
+      <a
+        href="#"
+        class="block px-4 py-2 text-sm text-gray-800 border-b hover:bg-gray-200"
+        >(seperator)</a
+      >
+    </div>
+
+    <div class="titlebar-center flex-grow" data-tauri-drag-region>
+      <span class="px-2" data-tauri-drag-region>タイトル</span>
+    </div>
+    <div class="titlebar-right flex-none">
+      <div class="titlebar-button" id="titlebar-pin" @click="onPinClick">
+        <img
+          src="https://api.iconify.design/mdi:pin-off.svg"
+          alt="pin-off"
+          v-if="alwaysOnTop"
+        />
+        <img src="https://api.iconify.design/mdi:pin.svg" alt="pin-on" v-else />
+      </div>
+      <div
+        class="titlebar-button"
+        id="titlebar-minimize"
+        @click="onMinimizeClick"
+      >
+        <img
+          src="https://api.iconify.design/mdi:window-minimize.svg"
+          alt="minimize"
+        />
+      </div>
+      <div
+        class="titlebar-button"
+        id="titlebar-maximize"
+        @click="onMaximizeClick"
+      >
+        <img
+          src="https://api.iconify.design/mdi:window-maximize.svg"
+          alt="maximize"
+        />
+        <!-- <img
+        src="https://api.iconify.design/mdi:window-restore.svg"
+        alt="restore"
+        /> -->
+      </div>
+      <div class="titlebar-button" id="titlebar-close" @click="onCloseClick">
+        <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+      </div>
+    </div>
   </div>
 </template>
-
 
 <style>
 .titlebar {
@@ -70,12 +98,13 @@ function onCloseClick(){
   background: #329ea3;
   user-select: none;
   display: flex;
-  justify-content: flex-end;
+
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
 }
+
 .titlebar-button {
   display: inline-flex;
   justify-content: center;
